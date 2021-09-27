0462f9e042fc4d55eeb6ee5b4196b0e9109ef7f9
Mehezabeen <mehezabeen.mahee2info@gmail.com>
2021-09-27 07:19:11 +0530

    changes

--- src/app/Component/template-form/template-form.component.ts
+++ src/app/Component/template-form/template-form.component.ts
@@ -8,7 +8,7 @@ import { Component, OnInit } from '@angular/core';
 export class TemplateFormComponent implements OnInit {
 
   constructor() { }
-
+  // gggggg
   ngOnInit(): void {
   }
   // onRegister(formData:any){
